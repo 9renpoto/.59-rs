f0455b4b4c1a173e45efcb905f13b91038e2dce0
Keisuke Umeno <9renpoto@gmail.com>
2023-06-18 12:04:02 +0900

    chore: skip coverage (#95)
    * :thinking:
    
    * :thinking:
    
    * feat: replace actions-rs
    
    * chore: use dtolnay rust-toolchain
    
    * chore: fmt
    
    * chore: collect coverage
    
    * chore: thinking about
    
    * chore: thinking about
    
    * chore: thinking about
    
    * chore: thinking about
    
    * fix: skip collect coverage

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@ fn main() {
 }
 
 pub fn prints_and_returns_10(a: i32) -> i32 {
-  //{}という値を得た
-  println!("I got the value {}", a);
-  10
+    //{}という値を得た
+    println!("I got the value {}", a);
+    10
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn this_test_will_pass() {
-      let value = prints_and_returns_10(4);
-      assert_eq!(10, value);
-  }
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
 }
